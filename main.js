e65175f84ec2f2fa552a616e1f21e5a550f9613e
Yinka <akinn.yinka@gmail.com>
2021-05-27 13:21:03 +0100

    completed project

---
+++ main.js
@@ -0,0 +1,39 @@
+//  alert("Click the switch to turn on the light");
+
+let buttonClick = document.querySelector("#switchBtn");
+let onBulb = document.querySelector(".onBulb");
+let switchState = false;
+
+
+buttonClick.addEventListener("click", btnSwitch)
+
+function btnSwitch(){
+    let switchInput = document.querySelector("#inputNum").value;
+    
+    if(switchInput){
+        let i = 1;
+        let interval = setInterval(()=>{
+            onBulb.style.display = "block"
+            setTimeout(()=>{
+                onBulb.style.display = "none"
+            }, 2000)
+            i++
+            if(i > switchInput) clearInterval(interval)
+            console.log(i);
+        }, 3000)
+    } else{ if (switchState){
+            switchState = false;
+            onBulb.style.display = "block";
+        } else {
+            switchState = true;
+            onBulb.style.display = "none";
+        }
+    }
+    } 
+
+
+
+
+
+
+
